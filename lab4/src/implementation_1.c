6e760d90c3127d0b6adf74798762bb97a7c236b1
bxusha <160039558+PashoktheprogrammisT@users.noreply.github.com>
2025-12-24 00:45:04 +0300

    Update implementation_1.c

--- lab4/src/implementation_1.c
+++ lab4/src/implementation_1.c
@@ -1,1 +1,30 @@
+#include <math.h>
+#include <stdlib.h>
 
+float Derivative(float A, float deltaX) {
+    return (cosf(A + deltaX) - cosf(A)) / deltaX;
+}
+
+int* Sort(int* array) {
+    int size = array[0];
+    int* data = array + 1;
+    
+    int* copy = (int*)malloc(size * sizeof(int));
+    if (!copy) return NULL;
+    
+    for (int i = 0; i < size; i++) {
+        copy[i] = data[i];
+    }
+    
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (copy[j] > copy[j + 1]) {
+                int temp = copy[j];
+                copy[j] = copy[j + 1];
+                copy[j + 1] = temp;
+            }
+        }
+    }
+    
+    return copy;
+}
