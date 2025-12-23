e249db27fc406ef5a2d7cbb21c4d35d63dbbde35
bxusha <160039558+PashoktheprogrammisT@users.noreply.github.com>
2025-12-24 00:44:15 +0300

    Update implementation_1.h

--- lab4/src/implementation_1.h
+++ lab4/src/implementation_1.h
@@ -1,1 +1,15 @@
+#ifndef IMPLEMENTATION_1_H
+#define IMPLEMENTATION_1_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+float Derivative(float A, float deltaX);
+int* Sort(int* array);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
